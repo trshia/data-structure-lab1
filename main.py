903b84281ee50d9140c7edfae3b902bafe068b1a
Trisha Morales <trishaannesegoviamorales@gmail.com>
2024-08-22 12:01:25 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# num0 = int(input("Enter first number: "))
+# num1 = int(input("Enter second number: "))
+#
+# if num0 > num1:
+#     print(f"{num0} is greater than {num1}")
+#
+# elif num0 > num1:
+#     print(f"{num0} is less than{num1}")
+#
+# elif num0 == num1:
+#     print(f"{num0}  is equals to {num1}")
+
+"""
+cm1 = input("Enter classmate name 1: ")
+cm2 = input("Enter classmate name 2: ")
+cm3 = input("Enter classmate name 3: ")
+
+print(f"Your  classmate are {cm1}, {cm2}, & {cm3}")
+"""
